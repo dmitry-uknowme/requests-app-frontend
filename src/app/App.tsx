b88fe9911f5ee5cc2830c0ed04a3aa1449244a65
Dmitry <bogatyrov200488@gmail.com>
2025-05-28 21:32:54 +0500

    add shadcn tailwind, fix config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/shared/ui/button";
 import { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-   
+    <div className="app">
+      <div className="app_title">
+        <h2 className="text-3xl font-semibold tracking-tight first:mt-0">
+          Список заявок
+        </h2>
+      </div>
+    </div>
   );
 };
 
